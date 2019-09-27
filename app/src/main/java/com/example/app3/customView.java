199e34af5b58b1667d568ef9383120a719c0a220
AMillik1 <amillik1@binghamton.edu>
2019-09-27 10:24:42 -0400

    added a new view

---
+++ app/src/main/java/com/example/app3/customView.java
@@ -0,0 +1,29 @@
+package com.example.app3;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.View;
+import android.graphics.Canvas;
+
+public class customView extends View {
+    public customView(Context context, AttributeSet attrs){
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        paint.setColor(Color.BLUE);
+        Rect rect = new Rect();
+        rect.left = 0;
+        rect.right = getWidth();
+        rect.top = 0;
+        rect.bottom = getHeight();
+
+        canvas.drawRect(rect, paint);
+    }
+}
