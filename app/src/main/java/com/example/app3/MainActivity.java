d889c58f45cc6476f2d3fa7a85db07e643182cd2
AMillik1 <amillik1@binghamton.edu>
2019-10-11 10:41:49 -0400

    Still working

--- app/src/main/java/com/example/app3/MainActivity.java
+++ app/src/main/java/com/example/app3/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.app3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.SurfaceView;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
@@ -20,32 +22,41 @@ public class MainActivity extends AppCompatActivity {
         //animation.setDuration(1000);
         //image.startAnimation(animation);
 
+        class dvdView extends SurfaceView implements Runnable{
+            Thread dvdThread = null;
+            float dx = 10;
+            float dy = 5;
+            float maxx = 500;
+            float maxy = 500;
+            float minx = 0;
+            float miny = 0;
+            float currx = 0;
+            float curry = 0;
 
-        float dx = 10;
-        float dy = 5;
-        float maxx = 500;
-        float maxy = 500;
-        float minx = 0;
-        float miny = 0;
-        float currx = 0;
-        float curry = 0;
-
-        while (true){
-            //run through and change the values and call animation
-            if (currx >= maxx || currx <= minx){
-                dx = -dx;
-            }
-            if (curry >= maxy || curry <= miny){
-                dy = -dy;
+            public dvdView(Context context){
+                super(context);
             }
 
-            Animation animation = new TranslateAnimation(currx, currx+dx, curry, curry+dy);
-            animation.setDuration(100);
-            currx += dx;
-            curry += dy;
+            while (true){
+                //run through and change the values and call animation
+                if (currx >= maxx || currx <= minx){
+                    dx = -dx;
+                }
+                if (curry >= maxy || curry <= miny){
+                    dy = -dy;
+                }
 
-            image.startAnimation(animation);
+                Animation animation = new TranslateAnimation(currx, currx+dx, curry, curry+dy);
+                animation.setDuration(100);
+                currx += dx;
+                curry += dy;
 
+                image.startAnimation(animation);
+                System.gc();
+
+            }
         }
+
+
     }
 }
